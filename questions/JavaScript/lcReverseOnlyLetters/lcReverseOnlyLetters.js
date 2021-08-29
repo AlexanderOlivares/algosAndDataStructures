87cdf18f782b6eb736ea818687174c2a3fb9efc0
alex olivares <alexander.b.olivares@gmail.com>
2021-08-29 16:56:45 -0500

    lcReverseOnlyLetters

---
+++ questions/JavaScript/lcReverseOnlyLetters/lcReverseOnlyLetters.js
@@ -0,0 +1,53 @@
+// LeetCode 917. Reverse Only Letters
+// tests in lcReverseOnlyLetters.test.js
+
+/* QUESTION:
+
+Given a string s, reverse the string according to the following rules:
+
+All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed.
+Return s after reversing it.
+
+*/
+
+///////////////////////
+// SOLUTION #1
+///////////////////////
+
+const reverseOnlyLetters = s => {
+  let arr = s.split("");
+  let letters = [];
+  for (let i = 0; i < s.length; i++) {
+    if (/[a-zA-Z]/.test(s[i])) {
+      letters.push(s[i]);
+      arr[i] = " ";
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === " ") {
+      arr[i] = letters.pop();
+    }
+  }
+  return arr.join("");
+};
+
+///////////////////////
+// SOLUTION #2
+///////////////////////
+
+const reverseOnlyLetters2 = s => {
+  const reg = new RegExp(/[a-zA-Z]/g);
+  const letters = s.match(reg);
+  let nonLetters = s.replace(reg, " ").split("");
+  for (let i = 0; i < nonLetters.length; i++) {
+    if (nonLetters[i] === " ") {
+      nonLetters[i] = letters.pop();
+    }
+  }
+  return nonLetters.join("");
+};
+
+module.exports = reverseOnlyLetters;
+module.exports = reverseOnlyLetters2;
---
+++ questions/JavaScript/lcReverseOnlyLetters/lcReverseOnlyLetters.test.js
@@ -0,0 +1,22 @@
+const reverseOnlyLetters = require("./lcReverseOnlyLetters");
+const reverseOnlyLetters2 = require("./lcReverseOnlyLetters");
+
+test("reverseOnlyLetters", () => {
+  expect(reverseOnlyLetters("ab-cd")).toEqual("dc-ba");
+  expect(reverseOnlyLetters("a-bC-dEf-ghIj")).toEqual("j-Ih-gfE-dCba");
+  expect(reverseOnlyLetters("Test1ng-Leet=code-Q!")).toEqual(
+    "Qedo1ct-eeLg=ntse-T!"
+  );
+  expect(reverseOnlyLetters("alexiscool")).toEqual("loocsixela");
+  expect(reverseOnlyLetters("-?()")).toEqual("-?()");
+});
+
+test("reverseOnlyLetters solution 2", () => {
+  expect(reverseOnlyLetters2("ab-cd")).toEqual("dc-ba");
+  expect(reverseOnlyLetters2("a-bC-dEf-ghIj")).toEqual("j-Ih-gfE-dCba");
+  expect(reverseOnlyLetters2("Test1ng-Leet=code-Q!")).toEqual(
+    "Qedo1ct-eeLg=ntse-T!"
+  );
+  expect(reverseOnlyLetters2("alexiscool")).toEqual("loocsixela");
+  expect(reverseOnlyLetters2("-?()")).toEqual("-?()");
+});
