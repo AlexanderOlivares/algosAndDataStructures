7442b00362bc197cef088bb5ad4197a3f0c37612
alex olivares <alexander.b.olivares@gmail.com>
2021-09-07 16:05:46 -0500

    lcValidParens

---
+++ questions/JavaScript/lcValidParens/lcValidParens.js
@@ -0,0 +1,58 @@
+// LeetCode 20. Valid Parentheses
+// tests in "./lcValidParens.test.js"
+
+/* QUESTION:
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+*/
+
+///////////////////////
+// SOLUTION #1
+///////////////////////
+
+const isValid = s => {
+  let map = new Map();
+  map.set("(", ")");
+  map.set("{", "}");
+  map.set("[", "]");
+
+  let matches = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      matches.push(s[i]);
+    } else {
+      if (map.get(matches.pop()) !== s[i]) {
+        return false;
+      }
+    }
+  }
+
+  return !matches.length;
+};
+
+///////////////////////
+// SOLUTION #2
+///////////////////////
+
+const isValid2 = s => {
+  let len = s.length / 2;
+  for (let i = 0; i < len; i++) {
+    s = s.replace(/\(\)/, "").replace(/\[\]/, "").replace(/\{\}/, "");
+  }
+  return !s.length;
+};
+
+module.exports = isValid;
---
+++ questions/JavaScript/lcValidParens/lcValidParens.test.js
@@ -0,0 +1,24 @@
+const isValid = require("./lcValidParens");
+const isValid2 = require("./lcValidParens");
+
+test("isValid", () => {
+  expect(isValid("()")).toEqual(true);
+  expect(isValid("()[]{}")).toEqual(true);
+  expect(isValid("(]")).toEqual(false);
+  expect(isValid("([)]")).toEqual(false);
+  expect(isValid("{[]}")).toEqual(true);
+  expect(isValid("([])")).toEqual(true);
+  expect(isValid("(([]){})")).toEqual(true);
+  expect(isValid("[")).toEqual(false);
+});
+
+test("isValid solution 2", () => {
+  expect(isValid2("()")).toEqual(true);
+  expect(isValid2("()[]{}")).toEqual(true);
+  expect(isValid2("(]")).toEqual(false);
+  expect(isValid2("([)]")).toEqual(false);
+  expect(isValid2("{[]}")).toEqual(true);
+  expect(isValid2("([])")).toEqual(true);
+  expect(isValid2("(([]){})")).toEqual(true);
+  expect(isValid2("[")).toEqual(false);
+});
