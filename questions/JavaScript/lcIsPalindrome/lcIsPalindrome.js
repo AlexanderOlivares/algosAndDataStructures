0aebda73ec4250ff665fcc9518c0e5d763241c84
alex olivares <alexander.b.olivares@gmail.com>
2021-09-08 21:30:16 -0500

    lcIsPalindrome

---
+++ questions/JavaScript/lcIsPalindrome/lcIsPalindrome.js
@@ -0,0 +1,61 @@
+// LeetCode 125. Valid Palindrome
+// tests in "./lcIsPalindrome.test.js"
+
+/* QUESTION:
+
+Given a string s, determine if it is a palindrome, considering
+only alphanumeric characters and ignoring cases.
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+
+*/
+
+///////////////////////
+// SOLUTION #1
+///////////////////////
+
+const isPalindrome = s => {
+  s = s.toLowerCase();
+  let reg = /[^a-z0-9]/i;
+
+  let st = 0;
+  let end = s.length - 1;
+
+  while (st <= end) {
+    if (reg.test(s[st])) {
+      while (reg.test(s[st])) {
+        st++;
+      }
+    }
+
+    if (reg.test(s[end])) {
+      while (reg.test(s[end])) {
+        end--;
+      }
+    }
+
+    if (s[st] !== s[end]) return false;
+    st++;
+    end--;
+  }
+  return true;
+};
+
+///////////////////////
+// SOLUTION #2
+///////////////////////
+
+const isPalindrome2 = s => {
+  let valid = s.toLowerCase().replace(/[^a-z0-9 ]/g, "");
+
+  if (valid.length === 1) return true;
+
+  valid = valid.replace(/ /g, "");
+
+  return valid === valid.split("").reverse().join("");
+};
+
+module.exports = isPalindrome;
---
+++ questions/JavaScript/lcIsPalindrome/lcIsPalindrome.test.js
@@ -0,0 +1,26 @@
+const isPalindrome = require("./lcIsPalindrome");
+const isPalindrome2 = require("./lcIsPalindrome");
+
+test("isPalindrome", () => {
+  expect(isPalindrome("race a car")).toEqual(false);
+  expect(isPalindrome("alexiscool")).toEqual(false);
+  expect(isPalindrome("alexxel")).toEqual(false);
+  expect(isPalindrome("A man, a plan, a canal: Panama")).toEqual(true);
+  expect(isPalindrome("a9a")).toEqual(true);
+  expect(isPalindrome(" ")).toEqual(true);
+  expect(isPalindrome("a")).toEqual(true);
+  expect(isPalindrome("00 00")).toEqual(true);
+  expect(isPalindrome("alexxela")).toEqual(true);
+});
+
+test("isPalindrome solution #2", () => {
+  expect(isPalindrome2("race a car")).toEqual(false);
+  expect(isPalindrome2("alexiscool")).toEqual(false);
+  expect(isPalindrome2("alexxel")).toEqual(false);
+  expect(isPalindrome2("A man, a plan, a canal: Panama")).toEqual(true);
+  expect(isPalindrome2("a9a")).toEqual(true);
+  expect(isPalindrome2(" ")).toEqual(true);
+  expect(isPalindrome2("a")).toEqual(true);
+  expect(isPalindrome2("00 00")).toEqual(true);
+  expect(isPalindrome2("alexxela")).toEqual(true);
+});
