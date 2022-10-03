bd61db948caa6386e2fb4ac3df29888f854b2360
alex olivares <alexander.b.olivares@gmail.com>
2022-10-03 18:07:13 -0500

    leet code 347. top k frequent elements second solution

--- questions/JavaScript/lcTopKFrequentElements/lcTopKFrequentElements.js
+++ questions/JavaScript/lcTopKFrequentElements/lcTopKFrequentElements.js
@@ -1,5 +1,5 @@
 // LeetCode 347. Top K Frequent Elements
-// tests in "./lcBuyAndSellStock.test.js"
+// tests in "./lcTopKFrequentElements.test.js"
 
 /* QUESTION:
 
@@ -25,6 +25,37 @@ const topKFrequent = (nums, k) => {
     map.set(i, map.get(i) + 1 || 1);
   }
 
+  console.log(map);
+  let buckets = Array.from({ length: nums });
+  for (let [key, val] of map.entries()) {
+    if (buckets[val]) {
+      buckets[val].push(key);
+    } else {
+      buckets[val] = [key];
+    }
+  }
+
+  let res = [];
+  for (let i = buckets.length; i >= 0; i--) {
+    if (buckets[i]) {
+      buckets[i].forEach(e => res.push(e));
+    }
+    // sorted for ease of testing
+    if (res.length === k) return res.sort();
+  }
+};
+
+///////////////////////
+// SOLUTION #2
+///////////////////////
+
+const topKFrequent2 = (nums, k) => {
+  let map = new Map();
+
+  for (let i of nums) {
+    map.set(i, map.get(i) + 1 || 1);
+  }
+
   let sorted = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
 
   let kFreq = [];
@@ -33,7 +64,11 @@ const topKFrequent = (nums, k) => {
     kFreq.push(sorted[i][0]);
   }
 
-  return kFreq;
+  // sorted for ease of testing
+  return kFreq.sort();
 };
 
-module.exports = topKFrequent;
+module.exports = {
+  topKFrequent,
+  topKFrequent2,
+};
--- questions/JavaScript/lcTopKFrequentElements/lcTopKFrequentElements.test.js
+++ questions/JavaScript/lcTopKFrequentElements/lcTopKFrequentElements.test.js
@@ -1,7 +1,19 @@
-const topKFrequent = require("./lcTopKFrequentElements.js");
+const { topKFrequent, topKFrequent2 } = require("./lcTopKFrequentElements.js");
 
 test("lcTopKFrequentElements solution #1", () => {
-  expect(topKFrequent([1, 1, 1, 2, 2, 3], 2)).toEqual([1, 2]);
+  expect(topKFrequent([1, 1, 1, 2, 2, 3], 2)).toEqual([1, 2].sort());
   expect(topKFrequent([1], 1)).toEqual([1]);
-  expect(topKFrequent([4, 8, 8, 3, 2, 8, 3, 1, 1, 1, 1], 3)).toEqual([1, 8, 3]);
+  expect(topKFrequent([4, 8, 8, 3, 2, 8, 3, 1, 1, 1, 1], 3)).toEqual([1, 8, 3].sort());
+  expect(topKFrequent2([4, 9, 18, 3, 22, 9, 3, 22, 3, 10, 11], 3)).toEqual([22, 3, 9].sort());
+  expect(topKFrequent2([4, 9, 18], 3)).toEqual([18, 4, 9].sort());
+  expect(topKFrequent2([9, 9, 18, 18, 18, 7], 2)).toEqual([18, 9].sort());
+});
+
+test("lcTopKFrequentElements solution #2", () => {
+  expect(topKFrequent2([1, 1, 1, 2, 2, 3], 2)).toEqual([1, 2].sort());
+  expect(topKFrequent2([1], 1)).toEqual([1]);
+  expect(topKFrequent2([4, 8, 8, 3, 2, 8, 3, 1, 1, 1, 1], 3)).toEqual([1, 8, 3].sort());
+  expect(topKFrequent2([4, 9, 18, 3, 22, 9, 3, 22, 3, 10, 11], 3)).toEqual([22, 3, 9].sort());
+  expect(topKFrequent2([4, 9, 18], 3)).toEqual([18, 4, 9].sort());
+  expect(topKFrequent2([9, 9, 18, 18, 18, 7], 2)).toEqual([18, 9].sort());
 });
